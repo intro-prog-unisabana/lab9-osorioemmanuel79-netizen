b2209319ada34f47f39cc97633d38c461f39b884
Emmanuel Osorio Bustacara <osorioemmanuel79@gmail.com>
2026-05-04 19:55:07 -0500

    sonv1

--- song.py
+++ song.py
@@ -1,1 +1,9 @@
 # Write your code here!
+class Song:
+    def __init__(self, name, artist, length):
+        self.name = name
+        self.artist = artist
+        self.length = length  # duración en minutos
+
+    def get_length_in_seconds(self):
+        return self.length * 60
