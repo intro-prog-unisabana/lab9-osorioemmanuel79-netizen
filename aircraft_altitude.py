79364437642af814418181cd11f0e8ddfe6d6853
Emmanuel Osorio Bustacara <osorioemmanuel79@gmail.com>
2026-05-04 20:47:34 -0500

    air

--- aircraft_altitude.py
+++ aircraft_altitude.py
@@ -1,11 +1,11 @@
 from aircraft import Aircraft
 
 def main():
-    model = input("Enter aircraft model:\n")
+    model = input("Enter aircraft model:")
     aircraft = Aircraft(model)
 
     while True:
-        command = input("Enter command (A for ascent, D for descent, X to exit):\n")
+        command = input("Enter command (A for ascent, D for descent, X to exit):")
 
         if command == "X":
             break
@@ -15,8 +15,11 @@ def main():
         if len(parts) != 2:
             continue
 
-        action = parts[0]
-        feet = int(parts[1])
+        action, value = parts
+        try:
+            feet = int(value)
+        except:
+            continue
 
         if action == "A":
             aircraft.ascent(feet)
